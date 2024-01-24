e9a3f52a01ac797c8d14f1359f8e892859454bee
Tanveer <shaikhtanveer5205@gmail.com>
2024-01-25 03:49:52 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
 
+connectDB()
+  .then(
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+      throw error;
+    })
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!", err);
+  });
 /*
 import express from "express";
 const app = express();
---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Resources not found",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+    this.data = null;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
